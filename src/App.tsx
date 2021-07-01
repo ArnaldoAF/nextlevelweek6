e8bdace69745441541f7c8fd4ee6362ead8ae10d
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2021-07-01 20:27:36 -0300

    rotas e navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { BrowserRouter, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom}/>
+    </BrowserRouter>
   );
 }
 
