60010b86485c3a35578d86a97937cc0fa53b0a0e
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2021-07-01 20:50:37 -0300

    autenticação com firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import {useHistory} from 'react-router-dom';
+import { auth, firebase} from '../services/firebase';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -10,8 +11,14 @@ import { Button } from '../components/Button';
 export function Home() {
     const history = useHistory();
 
-    function naviteToNewRoom() {
-        history.push('/rooms/new');
+    function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+            history.push('/rooms/new');
+        })
+        //history.push('/rooms/new');
     }
 
     return(
@@ -24,7 +31,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="LetMeAsk" />
-                    <button onClick={naviteToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
